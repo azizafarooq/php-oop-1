97ea54c3a28e343d17133c3ffe850d7b53bc4624
azizafarooq <azizafarooq2000@gmail.com>
2024-03-05 16:40:48 +0100

    feat: functions

--- index.php
+++ index.php
@@ -43,20 +43,7 @@ var_dump($first_movie, $second_movie);
   </head>
   <body>
     <div class="container mt-5">
-        <h1>Movies</h1>
-        <thead>
-            <tr>
-                <th>Movie</th>
-                <th>Language</th>
-                <th>Rating</th>
-            </tr>
-        </thead>
-        <tbody>
-            <th>
-                <?php 
-                ?>
-            </th>
-        </tbody>
+       
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
